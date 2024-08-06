d68a836f412e5f3d4df88b9a83084db74752f2c8
Grey_Shadow <149694081+greyshadow37@users.noreply.github.com>
2024-08-07 01:55:10 +0530

    update calc.py issues: 1

--- calc.py
+++ calc.py
@@ -1,20 +1,26 @@
 import numpy as np
 
-def single_matrix(arr,oper):
-    arr = np.array(arr)
-    if oper == 1:
-        return np.linalg.det(arr)
-    if oper == 2:
-        return np.linalg.inv(arr)
-    if oper == 3:
-        return np.linalg.eigvals(arr)
-    else:
-        return "Error; Invalid operation"
+def square_matrix(arr):
+    return len(arr) == len(arr[0])
+
+def matrix_dim(arr1,arr2):
+    return np.shape(arr1) == np.shape(arr2)
     
+def single_matrix(arr,oper):
+
+    try:
+        if oper == 1:
+            return np.linalg.det(arr)
+        if oper == 2:
+            return np.linalg.inv(arr)
+        if oper == 3:
+                return np.linalg.eigvals(arr)
+        else:
+            return "Error; Invalid operation"
+    except np.linalg.LinAlgError as e:
+        return f'error:{e}'
 
 def two_matrix(arr1,arr2,oper):
-    arr1 = np.array(arr1)
-    arr2 = np.array(arr2)
     if oper == 1:
         return np.add(arr1,arr2)
     if oper == 2:
@@ -23,19 +29,27 @@ def two_matrix(arr1,arr2,oper):
         return "Error; Invalid operation"
     
     
-def matrix_input():
+def master():
+    
     nmat=int(input('''1. Single matrix operations (square matrices only)
 2. Two matrix operations (matrices should be of same dimension)
 '''))
-    
     
     if nmat==1:
         dim=int(input('Enter the dimensions of the square matrix:\n'))
         l=[]
         for i in range(dim):
-            st=input('Enter the row seperated by commas:\n')
+            st=input(f'Enter the {i+1}th row seperated by commas:\n')
             temp = [int(x) for x in st.split(',')]
             l.append(temp)
+        
+        if not square_matrix(l):
+            return "Error: Matrix is not square"
+
+        #converting to numpy array
+        l=np.array(l)
+        
+        #taking choice of operation as input
         choice=int(input('''1. Determinant
 2. Inverse
 3. Eigenvalues
@@ -46,24 +60,31 @@ def matrix_input():
         dim=int(input('Enter the number of rows of the matrices:\n'))
         l1=[],l2=[]
         for i in range(dim):
-            st=input('Enter the row seperated by commas:\n')
+            st=input(f'Enter {i+1}th the row seperated by commas:\n')
             temp = [int(x) for x in st.split(',')]
             l1.append(temp)
         
         for i in range(dim):
-            st=input('Enter the row seperated by commas:\n')
+            st=input(f'Enter the {i+1}th row seperated by commas:\n')
             temp = [int(x) for x in st.split(',')]
             l2.append(temp)
         
+        
+        # condition to check whether matrix dimensions are same or not 
+        if not matrix_dim(l1, l2):
+            return "Error: Matrices have different dimensions"
+    
+        #converting to numpy arrays
+        l1=np.array(l1)
+        l2=np.array(l2)
+
+        #taking choice of operation as input
         choice=int(input('''1. Addition 
 2. Substraction
                      '''))
+        
         print(two_matrix(l1,l2,choice)) 
     
-    else:
-        print('''invalid option
-please try again
-                ''')
-        matrix_input()
 
-matrix_input()
+if __name__ == "__main__":
+    master()
